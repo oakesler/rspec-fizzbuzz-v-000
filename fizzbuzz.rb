c00756ad1592aa53d5a11f685b02cd48bec2dd4a
Olivia Kesler <oakesler@protonmail.ch>
2019-05-14 05:35:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
-  end
-  elsif int % 5 == 0 
-  "Buzz"
-  end
+    end
+  if int % 5 == 0 
+    "Buzz"
+    end
   elsif int % 3 && int % 5 
   "Fizzbuzz"
   end
